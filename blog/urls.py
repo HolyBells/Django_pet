452cac978fa696e92213c6fb46a9c5d7410abbf9
jason <jason1prod@mail.ru>
2022-10-04 14:29:16 +0300

    add some_info

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views as views_blog
 urlpatterns = [
     path('', views_blog.index),
     path('kianu', views_blog.kianu),
+    path('some_info', views_blog.get_guinness_world_records),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,14 @@ def kianu(request):
     data = {'year_born': 1992,
             'city_born': 'Kazan',
             'movie_name': 'BIG',
-            }
-    return render(request, 'blog/kianu.html', data)
+    }
+    return render(request, 'blog/kianu.html', context=data)
+
+
+def get_guinness_world_records(request):
+    context = {
+        'power_man': 'Narve Laeret',
+        'bar_name': 'Bob’s BBQ & Grill',
+        'count_needle': 1790,
+    }
+    return render(request, 'blog/guinnessworldrecords.html', context=context)
